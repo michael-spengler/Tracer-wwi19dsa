05a178a4685714e42adde4414bc54b7e1cb59cf9
DESKTOP-QTHGL3O\Xsino <heller.nico@gmx.de>
2021-01-10 14:47:10 +0100

    added Buttons changed Logo

--- vue frontend/tracer/src/App.vue
+++ vue frontend/tracer/src/App.vue
@@ -1,10 +1,27 @@
 <template>
-  <img alt="Vue logo" src="./assets/Tracer_icon.png">
+
+  <div>
+    <img id="Logo" alt="Vue logo" src="./assets/Tracer_icon.png" width="200" height="200">
+    <div>
+         <button class="button1" alt="Button1" v-on:click="b1"> Hallo </button>
+         <button class="button1" alt="Button2" v-on:click="b2"> Hallo2 </button>
+         <button class="button1" alt="Button3" v-on:click="b3"> Hallo3 </button>
+    </div>
+  </div>
+
 </template>
 
 <script>
 export default {
   name: 'App',
+  methods: {
+    b1() {
+    },
+    b2() {
+    },
+    b3() {
+    },
+  }
 }
 </script>
 
@@ -15,6 +32,22 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 50px;
+
 }
+.button1{
+  background-color: khaki;
+  margin: 20px;
+  text-align: center;
+  padding: 5px;
+  cursor: pointer;
+  transition: background .2s ease-in-out;
+  float:left
+
+}
+
+#Logo{
+  float: left
+}
+
 </style>
