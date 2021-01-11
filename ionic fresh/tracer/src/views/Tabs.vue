9366edc61e737726a594ab4a5ab421b2e3d8551b
lukasbach00 <lukas.bach1@gmx.de>
2021-01-11 14:01:56 +0100

    update on vue frontend

--- ionic fresh/tracer/src/views/Tabs.vue
+++ ionic fresh/tracer/src/views/Tabs.vue
@@ -25,6 +25,7 @@
 import { IonTabBar, IonTabButton, IonTabs, IonLabel, IonIcon, IonPage } from '@ionic/vue';
 import { ellipse, square, triangle } from 'ionicons/icons';
 
+
 export default {
   name: 'Tabs',
   components: { IonLabel, IonTabs, IonTabBar, IonTabButton, IonIcon, IonPage },
