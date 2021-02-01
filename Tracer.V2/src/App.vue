46488224fba163b2d9be6770403c7d4dbd5dce71
DESKTOP-QTHGL3O\Xsino <heller.nico@gmx.de>
2021-02-01 17:00:46 +0100

    Logo on Homescreen Some Bugfixes downgrading qrcode.vue to 1.7.0 becasue of runtimecompile is needed on higher stages

--- Tracer.V2/src/App.vue
+++ Tracer.V2/src/App.vue
@@ -14,5 +14,4 @@
   color: #2c3e50;
 }
 
-
 </style>
--- Tracer.V2/src/Pages/homescreen.vue
+++ Tracer.V2/src/Pages/homescreen.vue
@@ -1,19 +1,44 @@
 <template>
   <div>
-  <navigation_bar></navigation_bar>
+    <div class="logo"><img src="Tracer.V2/src/assets/Tracer_icon_vertical.svg" alt="error picture not available"/></div>
+    <div><navigation_bar></navigation_bar></div>
+    <div><setting_button :onclick="to_settings">Settings</setting_button></div>
+    <div><button_basicstyle :onclick="to_tracerEvent">Tracer Event erstellen</button_basicstyle></div>
+    <div><button_basicstyle :onclick="to_reportcase">Fall melden</button_basicstyle></div>
   </div>
 </template>
 
 <script>
 
 import navigation_bar from "@/components/navigation_bar";
+import setting_button from "@/components/setting_button";
+import button_basicstyle from "@/components/button_basicstyle";
+
 
 export default {
-name: "homescreen",
-  components: {navigation_bar},
+  name: "homescreen",
+  components: {button_basicstyle, setting_button, navigation_bar},
+  methods: {
+    to_settings() {
+      this.$router.push({path: "/settings"})
+    },
+    to_tracerEvent() {
+      this.$router.push({path: "/create_event"})
+    },
+    to_reportcase() {
+      this.$router.push({path: "/report_case"})
+    },
+  }
 }
+
+
+
+
 </script>
 
 <style scoped>
 
+.logo{
+
+}
 </style>
