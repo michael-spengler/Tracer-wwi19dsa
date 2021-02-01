e0aa0c57da2b77c29f488c4ae6404eb4f59eaff6
lukasbach00 <lukas.bach1@gmx.de>
2021-02-01 13:08:37 +0100

    add confirm_button component

--- Tracer.V2/src/Pages/create_event.vue
+++ Tracer.V2/src/Pages/create_event.vue
@@ -2,18 +2,23 @@
   <div>Tracer Event erstellen
    <div>
      <button_basicstyle class="buttonbasic"  v-on:click="onclick">
-      Freunde hinzufügen
+      Location auswählen
      </button_basicstyle>
-   </div>
+
+      <button_confirm class="buttonconfirm"  v-on:click="onclick">
+        Event erstellen
+      </button_confirm>
+  </div>
   </div>
 </template>
 
 <script>
 import button_basicstyle from "@/components/button_basicstyle";
+import button_confirm from "@/components/confirm_buttonstyle";
 
 export default {
 name: "create_event",
-  components: {button_basicstyle}
+  components: {button_confirm, button_basicstyle}
 }
 </script>
 
