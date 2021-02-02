4cd97ecff35891bbd913dec49f76f7dae6688193
unknown <GPaschaloglou@gmx.de>
2021-02-02 14:43:39 +0100

    report_case und fill_in_formular

--- Tracer.V2/src/Pages/report_case.vue
+++ Tracer.V2/src/Pages/report_case.vue
@@ -1,12 +1,35 @@
 <template>
-  <div>
-    hallo
+  <div>Fall melden
+    <div>
+
+      <b-card
+          style="max-width: 20rem;"
+          class="mb-2"
+      >
+        <b-card-text>
+          Falls Sie positiv auf Covid-19 getestet <br>
+          wurden, laden Sie hier Ihren Testbericht <br>
+          samt Unterschrift des Hausarztes hoch.
+        </b-card-text>
+      </b-card> <br>
+      <button_basicstyle class="buttonbasic"  v-on:click="onclick">
+        PDF Dokument oder JPG hochladen
+      </button_basicstyle> <br>
+      <button_basicstyle :onclick="caseroute">Formular ausfüllen</button_basicstyle>
+    </div>
   </div>
 </template>
-
 <script>
+import button_basicstyle from "@/components/button_basicstyle";
+
 export default {
-name: "Fall melden"
+  name: "Fall melden",
+  components: {button_basicstyle},
+  methods: {
+    caseroute() {
+      this.$router.push({path: '/fill_in_formular'});
+    }
+  }
 }
 </script>
 
