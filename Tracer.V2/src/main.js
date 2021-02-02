c3690bac88ac97f27aee1f085a18438724735bcc
DESKTOP-QTHGL3O\Xsino <heller.nico@gmx.de>
2021-02-02 12:04:03 +0100

    PWA feature integrated

--- Tracer.V2/src/main.js
+++ Tracer.V2/src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import VueQrcodeReader from "vue-qrcode-reader";    // NOTE: This line is very important!
 import router from './router'
+import './registerServiceWorker'
 
 Vue.config.productionTip = false
 Vue.use(VueQrcodeReader);                           // NOTE: This line is very important!
