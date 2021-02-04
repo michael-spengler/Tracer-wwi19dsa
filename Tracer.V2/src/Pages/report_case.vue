ece40984ee962a1f9f4787e6c13df389020040fd
DESKTOP-QTHGL3O\Xsino <heller.nico@gmx.de>
2021-02-04 01:07:15 +0100

    fixes

--- Tracer.V2/src/Pages/report_case.vue
+++ Tracer.V2/src/Pages/report_case.vue
@@ -11,38 +11,28 @@
           wurden, laden Sie hier Ihren Testbericht <br>
           samt Unterschrift des Hausarztes hoch.
         </b-card-text>
-      </b-card> <br>
-      <button_basicstyle class="buttonbasic"  v-on:click="onclick">
+      </b-card>
+      <br>
+      <button_basicstyle class="buttonbasic" v-on:click="onclick">
         PDF Dokument oder JPG hochladen
-      </button_basicstyle> <br>
+      </button_basicstyle>
+      <br>
       <button_basicstyle :onclick="caseroute">Formular ausfüllen</button_basicstyle>
-    </div>
-    <div>
-      <vue-pull-refresh :on-refresh="onRefresh"></vue-pull-refresh>
     </div>
   </div>
 </template>
 <script>
 import button_basicstyle from "@/components/button_basicstyle";
-import VuePullRefresh from 'vue-pull-refresh';
+
 
 export default {
   name: "Fall melden",
-  components: {button_basicstyle, VuePullRefresh},
+  components: {button_basicstyle},
   methods: {
     caseroute() {
       this.$router.push({path: '/fill_in_formular'});
     },
-    onRefresh: function () {
-      return new Promise(function (resolve) {
-        setTimeout(function () {
-          resolve();
-        }, 1000);
-      });
-    }
-  },
-  data: function () {
-    return {};
+
   },
 }
 
