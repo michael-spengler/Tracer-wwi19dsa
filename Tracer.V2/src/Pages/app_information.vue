759c02d5a91f94e7142ab590a1df6e96cbe82fc6
lukasbach00 <lukas.bach1@gmx.de>
2021-02-06 19:22:41 +0100

    add App Information

--- Tracer.V2/src/Pages/app_information.vue
+++ Tracer.V2/src/Pages/app_information.vue
@@ -1,13 +1,80 @@
 <template>
-<div><div><tab_bar></tab_bar></div></div>
+<div>
+
+  <h1 class="h1">Information Text</h1>
+  <p class=p>Tracer ermöglicht einfaches und anonymes nachverfolgen von Infektionsketten.
+
+    Mit Tracer können Sie einfachy bequem und schnell Infektionsketten nachverfolgen. Einfach den QR-Code des Restaurants oder Geschäfts scannen.
+
+    Dadurch können händische Zettel oder Listen abgeschafft werden. Es spart Zeit und keine sensiblen Daten werden veröffentlicht.</p>
+  <h1 class="h1">FAQ:</h1>
+  <h2 class="h2">Wie funktioniert Tracer?</h2>
+  <p class=p>Jeder Besucher scannt einen QR-Code mit der Tracer-App. Dadurch wird der Ort und der Zeitpunkt des Besuchs einer Datenbank übermittelt. Diese werden zusammen mit einer zufälligen aber eindeutigen Nummer versehen. Diese Nummer wird anschließend auch auf Ihrem Endgerät gespeichert.
+
+    Meldet nun eine Person einen positiven Fall, können alle auf deren Endgerät gespeicherten Nummern der Datenbank übergeben werden. Daraus kann der Ort und der Zeitpunkt potentieller Infektionen herausgefunden werden. Alle Nummern, die für den selben Ort zu ungefähr dem selben Zeitraum angelegt worden, werden veröffentlicht. Die Tracer App überprüft ob eine auf dem eigenen Endgerät gespeicherten Nummer betroffen ist. Ist dies der Fall wird der Nutzer in Form einer roten Meldung informiert.
+
+    Weitere Informationen finden Sie <a href="https://github.com/michael-spengler/Tracer-wwi19dsa">hier</a>
+  </p>
+  <h2 class="h2">
+    Welche Daten werden erhoben und ist es wirklich anonym?
+  </h2>
+  <p class=p>
+    Die Tracer App braucht lediglich eine Kamera und Speicherplatz für die auf dem Engerät gespeicherten Nummern.
+    Im Vergleich zu passiven Tracking-Apps werden keine Sensoren wie Bluetooth benötigt. Jedoch wird auch nur getrackt, wenn ein QR-Code aktiv getrackt wird.
+    In der zugrundeliegenden App werden lediglich der Ort (als zufälliger eindeutiger Wert), ein Zeitpunkt und eine zufällige eindeutige Nummer gespeichert (User ID).
+    Es gibt keine Möglichkeit einen Eintrag eines Nutzers mit einem weiteren Eintrag des Selben Nutzers zu verknüpfen. Die einzigartigen Nummern werden immer zentral von der Datenbank generiert und dem Endgerät übertragen.
+    Nicht mehr benötigte Daten werden ebenfalls gelöscht.
+    Tracer schützt jedoch nicht vor komprimierten Internetverbindungen oder ähnlichem.
+
+    Weitere Informationen finden Sie <a href="https://github.com/michael-spengler/Tracer-wwi19dsa">hier</a>
+  </p>
+  <h2 class="h2">
+    Wie kann ich eigene QR-Codes erstellen?
+  </h2>
+  <p class=p>
+    Wenn Sie einen eigenes Geschäft haben oder im Rahmen einer Veranstaltung Infektionsketten nachverfolgen wollen können Sie mithilfe der Tracer-App ebenfalls einen eigenen QR-Code erstellen. Dafür müssen Sie nur eine übliche Verweildauer angeben (z.B. Supermarkt 30 Minuten) und anschließend wird auf dem Server eine zufällige eindeutige Nummer erzeugt und für Sie in Form eines QR-Codes bereitgestellt. Anschließend können Sie mit dem Teilen beginnen. Beachten Sie, dass Sie diesen QR-Code sichern müssen. Tracer speichert diesen nicht.
+  </p>
+  <h2 class="h2">
+    Wer steckt hinter Tracer?
+  </h2>
+  <p class=punten>
+    Tracer ist ein studentisches Projekt von 6 dualen Wirtschaftsinformatik Studenten der DHBW Mannheim. Das Projekt ist OpenSource und unterliegt der MIT Lizenz. Für weitere Informationen schauen Sie <a href="https://github.com/michael-spengler/Tracer-wwi19dsa">hier</a>
+  </p>
+  <tab_bar></tab_bar>
+  </div>
 </template>
 
 <script>
+import tab_bar from "@/components/tab_bar";
+
 export default {
+  components: {tab_bar},
   name: "app_information"
 }
 </script>
 
 <style scoped>
-
+.p{
+  font-family: Roboto;
+  color: #676767;
+  margin: 30px;
+}
+.h1{
+  font-family: Roboto;
+  color: #676767;
+  padding: 15px;
+}
+.h2{
+  font-family: Roboto;
+  color: #676767;
+  padding: 15px;
+}
+.punten{
+  padding-bottom: 100px;
+  padding-left: 15px;
+  padding-right: 15px;
+  padding-top: 15px;
+  font-family: Roboto;
+  color: #676767;
+}
 </style>
--- Tracer.V2/src/Pages/settings.vue
+++ Tracer.V2/src/Pages/settings.vue
@@ -1,6 +1,15 @@
 <template>
   <div>
     <tab_bar></tab_bar>
+    <div class="homescreen_button">
+      <v-btn
+          color= "#99D7F0"
+          dark
+          width="85%"
+          v-on:click="to_app_information">
+        App Information
+      </v-btn>
+    </div>
   </div>
 </template>
 
@@ -9,7 +18,13 @@ import tab_bar from "@/components/tab_bar";
 
 export default {
   components: {tab_bar},
-  name: "Settings"
+  name: "Settings",
+  methods: {
+    to_app_information() {
+      this.$router.push({path: "/app_information"})
+
+    },
+  }
 }
 </script>
 
