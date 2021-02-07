0abf1f8d1af362e2b6095918882a60a712babd6c
BennerLukas <lukas_benner@outlook.de>
2021-02-07 11:24:50 +0100

    design idea event_erstellen

--- Tracer.V2/src/Pages/create_event.vue
+++ Tracer.V2/src/Pages/create_event.vue
@@ -1,5 +1,18 @@
 <template>
-  <div>Tracer Event erstellen
+  <div>
+    <br><br>
+    <img class="logo" src="@/assets/qr_code-24px.svg" height="70" alt="error picture not available"/>
+    <br>
+    <h2>Tracer Event erstellen</h2>
+     <div>
+       <br>
+       <p>
+         Bitte geben Sie die durschnitliche Verweildauer Ihrer Kunden in Minuten an. Diese wird benötigt um abschätzen zu können welche Kunden bei einer möglichen Infektion kontaktiert werden müssen.
+       </p>
+      <div style="margin:50px ">
+        <v-text-field label="Verweildauer in Minuten" :rules="rules" hide-details="auto" ></v-text-field>
+      </div>
+  </div>
     <div>
       <button_basicstyle :onclick="to_generate">Qr-Code erzeugen</button_basicstyle>
     </div>
@@ -19,9 +32,37 @@ export default {
       this.$router.push({path:'/qr_generator'})
     },
   },
+   data: () => ({
+      rules: [
+        value => !!value || 'Required.',
+        value => (value && value.length >= 1) || 'Min 1 characters',
+      ],
+    }),
 }
 </script>
 
 <style scoped>
-
+.h2{
+  font-family: Roboto;
+  color: #676767;
+  padding: 15px;
+}
+.span{
+  display:block;
+  height:200px;
+  width:100%;
+  background:white;
+}
+.untercard{
+  display:block;
+  height:30px;
+  width:100%;
+  background:white;
+}
+.p{
+  font-family: Roboto;
+  color: #676767;
+  margin: 30px;
+  text-align: left;
+}
 </style>
