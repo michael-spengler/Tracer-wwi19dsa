72856062b35c2290e3a294d593f4e06ba00af456
Madhour <aymanmadhour@gmail.com>
2021-02-08 02:24:14 +0100

    enabled cros

--- server/src/deps.ts
+++ server/src/deps.ts
@@ -2,6 +2,7 @@
 export { opine, serveStatic } from "https://deno.land/x/opine@0.25.0/mod.ts";
 export { cuid } from 'https://deno.land/x/cuid/index.js';
 export { Client } from 'https://deno.land/x/mysql/mod.ts';
+export { opineCors } from "https://deno.land/x/cors@v1.2.1/opineCors.ts";
 
 //outdated dependencies
 export { writeJsonSync } from 'https://deno.land/x/jsonfile/mod.ts';
