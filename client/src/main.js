db2ca7941c92e1610f1d5986778eaa8b0cbaf674
lukasbach00 <lukas.bach1@gmx.de>
2021-02-18 13:14:50 +0100

    add loading screen to refresh buttom !WIP!

--- client/src/main.js
+++ client/src/main.js
@@ -4,10 +4,15 @@ import VueQrcodeReader from "vue-qrcode-reader";    // NOTE: This line is very i
 import router from './router'
 import wb from "./registerServiceWorker";
 import vuetify from '@/plugins/vuetify' // path to vuetify export
+import Loading from 'vue-loading-overlay';
+import 'vue-loading-overlay/dist/vue-loading.css';
 
 Vue.prototype.$workbox = wb;
 Vue.config.productionTip = false
 Vue.use(VueQrcodeReader);                           // NOTE: This line is very important!
+Vue.use(Loading, {
+  color: 'green'
+})
 
 new Vue({
   vuetify,
